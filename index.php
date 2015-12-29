5e357ff14119040474f45d81fe6152c89caf87ad
sethearn1234 <gamedevsignup@gmail.com>
2015-12-29 14:07:41 -0800

    added latest files
    added the latest files

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE HTML>
+<!--
+	Solid State by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Procul Games - Home</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">Procul Games</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="about us.html">About us</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo">
+							  <p><img src="images/Procul-Games.png" width="200" height="160" alt="Procul Games Logo"></p>
+							</div>
+							<h2>procul games</h2>
+							<p>We are a small indie development team, trying to gain experience and skills by developing games.</p>
+						</div>
+					</section>
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+						<!-- One -->
+							<section id="one" class="wrapper spotlight style1">
+								<div class="inner">
+									<a href="http://www.twitter.com/proculgamedev" target="_blank" class="image"><img src="images/Twitter.png" alt="" width="1139" /></a>
+									<div class="content">
+									  <h2 class="major">twitter</h2>
+										<p>Follow us on Twitter for updates on any games that we are making and will make in the future. Feel free to come leave feedback so that we can continue to improve our games and improve our game-developing skills!</p>
+										<a href="http://www.twitter.com/proculgamedev" target="_blank" class="special">VISIT OUR TWITTER</a>
+								  </div>
+							  </div>
+							</section>
+
+						<!-- Two -->
+							<section id="two" class="wrapper alt spotlight style2">
+								<div class="inner">
+									<a href="https://www.youtube.com/channel/UCVjTxhUg94609b0YKLLSsdw" class="image"><img src="images/Youtube_Play.png" alt="" /></a>
+								  <div class="content">
+									<h2 class="major">Youtube</h2>
+										<p>Why not visit our YouTube. We will post update videos showing progression within our games and what everyone can expect in future updates/patches. It is a good place to start if you are looking for updates on our games!</p>
+										<a href="https://www.youtube.com/channel/UCVjTxhUg94609b0YKLLSsdw" class="special">VISIT OUR YOUTUBE</a>
+									</div>
+							  </div>
+							</section>
+
+						<!-- Three -->
+							<section id="three" class="wrapper spotlight style3">
+								<div class="inner">
+									<a href="https://www.facebook.com/proculgames/" class="image"><img src="images/Facebook.png" alt="" /></a>
+									<div class="content">
+									  <h2 class="major">Facebook</h2>
+									  <p>Like us on Facebook. Here you can stay up to date on our games by reading full changelogs. You could also have a chance of winning a free copy of our game if we do a giveaway!</p>
+										<a href="https://www.facebook.com/proculgames/" class="special">VISIT OUR FACEBOOK</a>
+								  </div>
+							  </div>
+							</section>
+
+						<!-- Four -->
+							<section id="four" class="wrapper alt style1">
+								<div class="inner">
+									<h2 class="major">Available positions</h2>
+									<p>Below are the position that are available within the team and how many of that position you are able to apply for.</p>
+									<section class="features">
+										<article>
+											<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+											<h3 class="major">Programmer/Developer</h3>
+											<p>&lt;No. of Slots&gt; Available!</p>
+											<p>You will be responsible for designing and creating some of the games core mechanics and features. These could include; Health, Hunger, AI, UI, Main Menu &amp; Pause Menu etc.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+											<h3 class="major">3D Modeller</h3>
+											<p>&lt;No. of Slots&gt; Available!</p>
+											<p>You will be responsible for creating 3D models for which they may be used within the game. These things could include, Characters, Houses/Buildings, Misc. Items, Furniture etc.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+											<h3 class="major">Level Designer</h3>
+											<p>&lt;No. of Slots&gt; Available!</p>
+											<p>You will be responsible for creating the landscape within the game and placing models within the world. You will have to position models to get the most out of the world and make it as atmospheric as possible.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+										  <h3 class="major">Sound &amp; Music Artist</h3>
+											<p>&lt;No. of Slots&gt; Available!</p>
+											<p>You will be responsible for creating sounds that we can put into the game. These may include but not limited to; Movement Sounds, Animal Noises and Ambient Music.</p>
+										</article>
+									</section>
+</div>
+							</section>
+
+					</section>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Apply Here</h2>
+							<p>Would you like to join the team? Interested in turning your passion for video games into a job? Why not start here! We are a small team that are motivated to make games so that we can progress and use the skills that we pick up along the way to continue to make games and possibly make money doing it!</p>
+							<form action="assets/php/send_form_email.php" method="post" id="ApplicationForm">
+								<div class="field">
+									<label for="FullName">Name</label>
+									<input type="text" name="FullName" id="FullName" />
+								</div>
+								<div class="field">
+									<label for="email">Email</label>
+									<input type="email" name="email" id="email" />
+								</div>
+								<div class="field">
+									<label for="message">Message + Position applying for</label>
+									<textarea name="message" id="message" rows="4"></textarea>
+								</div>
+								<ul class="actions">
+									<li><input name="Submit" type="submit" id="Submit" value="Send Message" /></li>
+								</ul>
+							</form>
+							<ul class="contact">
+							  <li class="fa-envelope"><a href="mailto:proculgamedevelopment@gmail.com" target="_blank">proculgamedevelopment@gmail.com</a></li>
+								<li class="fa-twitter"><a href="http://www.twitter.com/proculgamedev" target="_blank">twitter.com/proculgamedev</a></li>
+								<li class="fa-facebook"><a href="http://www.facebook.com/proculgames" target="_blank">facebook.com/proculgames</a></li>
+                                <li class="fa-youtube"><a href="https://www.youtube.com/channel/UCVjTxhUg94609b0YKLLSsdw" target="_blank">youtube.com/proculgames</a></li>
+							</ul>
+</div>
+					</section>
+
+			</div>
+		<!-- Scripts -->
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/backtotop.js"></script>
+	</body>
+</html>
